1888279f5aa20dcec4a26b50646f1997b1954995
Christoph Böhner-Figas <boehner@team23.de>
2022-02-19 13:14:11 +0100

    feat(): Update tracking to sdk version

--- resources/theme/mhb.theme/scripts/app.js
+++ resources/theme/mhb.theme/scripts/app.js
@@ -32,26 +32,26 @@ if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
     document.documentElement.className += " u-device--ios";
 };
 
-let sendTelemetry = function (signalType) {
-    fetch('https://nom.telemetrydeck.com/api/v1/apps/6D371B8B-9F26-4CCC-874D-C7263F1B1481/signals/', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "clientUser": navigator.userAgent,
-            "type": signalType,
-            "payload": {
-                "url": window.location.href,
-                "useragent": navigator.userAgent,
-                "language": navigator.language,
-                "platform": navigator.platform,
-                "vendor": navigator.vendor,
-            }
-        })
-    });
-}
-
-sendTelemetry("pageLoad")
+// let sendTelemetry = function (signalType) {
+//     fetch('https://nom.telemetrydeck.com/api/v1/apps/6D371B8B-9F26-4CCC-874D-C7263F1B1481/signals/', {
+//         method: 'POST',
+//         mode: 'cors',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             "clientUser": navigator.userAgent,
+//             "type": signalType,
+//             "payload": {
+//                 "url": window.location.href,
+//                 "useragent": navigator.userAgent,
+//                 "language": navigator.language,
+//                 "platform": navigator.platform,
+//                 "vendor": navigator.vendor,
+//             }
+//         })
+//     });
+// }
+//
+// sendTelemetry("pageLoad")
